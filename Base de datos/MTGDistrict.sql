e8de30e105c8ddb8b4ae41dd757785c5cf92772a
sopa <david.r.r.r.25.05@gmail.com>
2025-05-04 19:16:17 +0200

    Modificacion de la base de datos

--- Base de datos/MTGDistrict.sql
+++ Base de datos/MTGDistrict.sql
@@ -1,14 +1,13 @@
---BORRADO DE LA BASE DE DATOS EN CASO DE QUE YA EXISTA
+-- BORRADO DE LA BASE DE DATOS EN CASO DE QUE YA EXISTA
 DROP DATABASE IF EXISTS MTGDistrictDB;
 
---CREACIÓN DE LA BASE DE DATOS
+-- CREACIÓN DE LA BASE DE DATOS
 CREATE DATABASE MTGDistrictDB;
 
---DECLARAMOS QUE VAMOS A USAR LA BASE DE DATOS PARA INSERTAR LAS SIGUIENTES TABLAS Y DATOS
+-- USO DE LA BASE DE DATOS
 USE MTGDistrictDB;
 
-
---TABLAS__TABLAS__TABLAS__TABLAS__TABLAS__TABLAS__TABLAS--
+-- TABLAS
 CREATE TABLE Usuario (
     IDUsuario INT AUTO_INCREMENT PRIMARY KEY,
     NombreUsuario VARCHAR(100) NOT NULL UNIQUE,
@@ -26,42 +25,61 @@ CREATE TABLE Mazo (
     FechaCreacionMazo TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     VisibilidadMazo TINYINT NOT NULL,
     VotacionesPositivasMazo VARCHAR(100) NOT NULL,
-)
+    FOREIGN KEY (IDUsuario) REFERENCES Usuario(IDUsuario)
+);
 
 CREATE TABLE Carta (
     IDCarta INT AUTO_INCREMENT PRIMARY KEY,
     NombreCarta VARCHAR(100) NOT NULL UNIQUE,
     ColorCarta VARCHAR(100) NOT NULL,
     CosteManaCarta INT NOT NULL,
-    ImagenUrlCarta VARCHAR(100) NOT NULL,
-)
+    ImagenUrlCarta VARCHAR(100) NOT NULL
+);
 
 CREATE TABLE MazoCarta (
-    IDMazo INT PRIMARY KEY,
+    IDMazo INT NOT NULL,
     IDCarta INT NOT NULL,
     Cantidad INT NOT NULL,
-)
+    PRIMARY KEY (IDMazo, IDCarta),
+    FOREIGN KEY (IDMazo) REFERENCES Mazo(IDMazo),
+    FOREIGN KEY (IDCarta) REFERENCES Carta(IDCarta)
+);
 
 CREATE TABLE Coleccion (
     IDColeccion INT AUTO_INCREMENT PRIMARY KEY,
     IDUsuario INT NOT NULL,
     NombreColeccion VARCHAR(100) NOT NULL,
     DescripcionColeccion VARCHAR(20) NOT NULL,
-    FechaCreacionColeccion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+    FechaCreacionColeccion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (IDUsuario) REFERENCES Usuario(IDUsuario)
+);
 
 CREATE TABLE ColeccionCarta (
-    IDColeccion INT PRIMARY KEY,
+    IDColeccion INT NOT NULL,
     IDCarta INT NOT NULL,
     Cantidad INT NOT NULL,
-)
+    PRIMARY KEY (IDColeccion, IDCarta),
+    FOREIGN KEY (IDColeccion) REFERENCES Coleccion(IDColeccion),
+    FOREIGN KEY (IDCarta) REFERENCES Carta(IDCarta)
+);
 
+-- INSERCIÓN DE DATOS
+INSERT INTO Usuario (NombreUsuario, EmailUsuario, PasswordUsuario)
+VALUES ("David", "david@mtg.com", "1234");
 
+INSERT INTO Mazo (IDUsuario, NombreMazo, DescripcionMazo, FormatoMazo, VisibilidadMazo, VotacionesPositivasMazo)
+VALUES ("1", "Mazo1", "primer mazo", "Estandar", 1, 12);
 
+INSERT INTO Carta (NombreCarta, ColorCarta, CosteManaCarta, ImagenUrlCarta)
+VALUES ("Kami", "Azul", 2, "urlinventada");
 
---INSERCIÓN_DE_DATOS__INSERCIÓN_DE_DATOS__INSERCIÓN_DE_DATOS--
-INSERT INTO usuarios (nombre, email, contrasena) VALUES
-('Ana García', 'ana@example.com', 'clave123'),
-('Luis Fernández', 'luis@example.com', 'pass456'),
-('Marta López', 'marta@example.com', 'secreta789');
+INSERT INTO MazoCarta (IDMazo, IDCarta, Cantidad)
+VALUES (1, 1, 4);
+
+INSERT INTO Coleccion(IDUsuario, NombreColeccion, DescripcionColeccion)
+VALUES (1, "Coleccion1", "DescripcionColeccion")
+
+INSERT INTO ColeccionCarta (IDColecion, IDCarta, Cantidad)
+VALUES (1, 1, 20);
+
 
