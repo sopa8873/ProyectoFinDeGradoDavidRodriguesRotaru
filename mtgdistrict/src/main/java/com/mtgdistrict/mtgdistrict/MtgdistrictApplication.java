93283afc4ffc75794c4bff38fd9f0ddc53c44f7f
sopa <david.r.r.r.25.05@gmail.com>
2025-05-04 20:26:36 +0200

    Creacion del Proyecto con SpringInitializrWeb

---
+++ mtgdistrict/src/main/java/com/mtgdistrict/mtgdistrict/MtgdistrictApplication.java
@@ -0,0 +1,13 @@
+package com.mtgdistrict.mtgdistrict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgdistrictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgdistrictApplication.class, args);
+	}
+
+}
