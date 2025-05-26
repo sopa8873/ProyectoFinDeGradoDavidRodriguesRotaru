f3212831157c5355e036c3477bdaeb8e16c91e5c
sopa8873 <david.rotaru2508@gmail.com>
2025-05-26 20:21:23 +0200

    intentando modificar el login de spring security

--- backend/src/main/java/com/mtgdistrict/config/SecurityConfig.java
+++ backend/src/main/java/com/mtgdistrict/config/SecurityConfig.java
@@ -14,7 +14,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/login", "/css/**").permitAll()
+                .requestMatchers("/", "/login", "/css/**", "/js/**", "/img/**", "/webjars/**", "/includes/**").permitAll()
+
                 .anyRequest().authenticated()
             )
             .formLogin(form -> form
